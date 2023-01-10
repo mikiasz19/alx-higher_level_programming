45f3896bfcd7f100dbecf62fc666cdac9d5362c6
Mikias zerihun <111016634+mikiasz19@users.noreply.github.com>
2023-01-10 05:41:24 +0300

    Update 8-main.py

--- 0x04-python-more_data_structures/tests/8-main.py
+++ 0x04-python-more_data_structures/tests/8-main.py
@@ -6,12 +6,11 @@ print_sorted_dictionary = \
 a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3] }
 new_dict = simple_delete(a_dictionary, 'track')
 print_sorted_dictionary(a_dictionary)
-print("--")
+print("___")
 print_sorted_dictionary(new_dict)
 
-print("--")
-print("--")
+print("___")
 new_dict = simple_delete(a_dictionary, 'c_is_fun')
 print_sorted_dictionary(a_dictionary)
-print("--")
+print("___")
 print_sorted_dictionary(new_dict)
