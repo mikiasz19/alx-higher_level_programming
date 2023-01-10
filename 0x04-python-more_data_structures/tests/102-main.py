c04f13ff04aa45787a039be7133053d4a61d3007
Mikias zerihun <111016634+mikiasz19@users.noreply.github.com>
2023-01-10 05:37:51 +0300

    Update 102-main.py

--- 0x04-python-more_data_structures/tests/102-main.py
+++ 0x04-python-more_data_structures/tests/102-main.py
@@ -6,12 +6,11 @@ print_sorted_dictionary = \
 a_dictionary = {'lang': "C", 'track': "Low", 'pref': "C", 'ids': [1, 2, 3]}
 new_dict = complex_delete(a_dictionary, 'C')
 print_sorted_dictionary(a_dictionary)
-print("--")
+print("___")
 print_sorted_dictionary(new_dict)
 
-print("--")
-print("--")
+print("___")
 new_dict = complex_delete(a_dictionary, 'c_is_fun')
 print_sorted_dictionary(a_dictionary)
-print("--")
+print("___")
 print_sorted_dictionary(new_dict)
