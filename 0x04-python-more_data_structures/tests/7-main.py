f0ff87a8a52fea6d7d3d47b3e5df94b660b4f531
Mikias zerihun <111016634+mikiasz19@users.noreply.github.com>
2023-01-10 05:40:50 +0300

    Update 7-main.py

--- 0x04-python-more_data_structures/tests/7-main.py
+++ 0x04-python-more_data_structures/tests/7-main.py
@@ -5,13 +5,12 @@ print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_d
 a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
 new_dict = update_dictionary(a_dictionary, 'language', "Python")
 print_sorted_dictionary(new_dict)
-print("--")
+print("___")
 print_sorted_dictionary(a_dictionary)
 
-print("--")
-print("--")
+print("___")
 
 new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
 print_sorted_dictionary(new_dict)
-print("--")
+print("___")
 print_sorted_dictionary(a_dictionary)
